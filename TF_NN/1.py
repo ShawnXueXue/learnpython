a0f508ed43deab8e4ed211c85180b79d13f29101
wgxue <wgxue@ctrip.com>
2018-07-30 15:29:13 +0800

    test

--- TF_NN/1.py
+++ TF_NN/1.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 # with tf.device('/cpu:0'):
 logdir='log_test/'
 def m1():
-    hello = tf.constant('Hello TF!')
+    hello =  tf.constant('Hello TF!')
     sess = tf.Session()
     tf.summary.FileWriter(logdir, sess.graph)
     print(sess.run(hello))
